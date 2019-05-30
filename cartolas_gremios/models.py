43a8efa86728843fd50f95ab0ce4d567c8b3b163
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2019-05-30 17:30:18 -0400

    add path to document model

--- cartolas_gremios/models.py
+++ cartolas_gremios/models.py
@@ -3,3 +3,4 @@ from django.db import models
 class Document(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    path = models.CharField(null=True, max_length=200)
