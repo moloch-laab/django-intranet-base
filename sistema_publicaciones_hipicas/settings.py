10c687c1b5e81dc2d2907267f8cd927ef68c54ac
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2019-05-30 16:02:32 -0400

    use sistema_publicaciones_hipicas database

--- sistema_publicaciones_hipicas/settings.py
+++ sistema_publicaciones_hipicas/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'sistema_publicaciones_hipicas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sistema_publicaciones_hipicas',
+        'USER': 'admin_hch',
+        'PASSWORD': 'pass.1234',
+        'HOST': '10.10.10.85',
+        'PORT': '5432',
     }
 }
 
@@ -103,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
