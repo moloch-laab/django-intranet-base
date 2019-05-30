44a2bc5fcda308e559300d32f6abec011bb31256
Francisco Bahamondes <francisco.bahamondes90@gmail.com>
2019-05-30 16:40:28 -0400

    model Document

--- cartolas_gremios/models.py
+++ cartolas_gremios/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Document(models.Model):
+    title = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
--- sistema_publicaciones_hipicas/settings.py
+++ sistema_publicaciones_hipicas/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cartolas_gremios.apps.CartolasGremiosConfig'
 ]
 
 MIDDLEWARE = [
