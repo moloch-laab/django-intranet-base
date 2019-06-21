cc5a495c440d33bb09720ce80eb5ea86630faddb
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2019-06-21 12:29:18 -0400

    load template files

--- core/menus.py
+++ core/menus.py
@@ -1,18 +1,25 @@
 MENUS = {
+    'MAIN_SIDEBAR': [
+        {
+            "name": "Gremios",
+            "icon": "fa-book",
+            "url": "#",
+            "validators": ["menu_generator.validators.is_authenticated"],
+            "submenu": [
+                {
+                    "name": "Cartolas de movimientos",
+                    "icon": "",
+                    "url": "/cartolas_gremios/",
+                },
+            ],
+        },
+    ],
     'NAVBAR': [
         {
             "name": "Inicio",
             "icon": "fa-home",
             "url": "/",
         },
-        {
-            "name": "Gremios",
-            "icon": "",
-            "url": "/cartolas_gremios/",
-            "validators": ["menu_generator.validators.is_authenticated"],
-        },
-    ],
-    'ACCOUNTS': [
         {
             "name": "Iniciar Sesión",
             "icon": "fa-sign-in-alt",
