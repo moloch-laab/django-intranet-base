199de6ecc1b4af02d50ba2825634bd7f42a8b3e1
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2019-06-21 17:07:02 -0400

    navbars ready

--- core/menus.py
+++ core/menus.py
@@ -20,29 +20,5 @@ MENUS = {
             "icon": "fa-home",
             "url": "/",
         },
-        {
-            "name": "Iniciar Sesión",
-            "icon": "fa-sign-in-alt",
-            "url": "/login/",
-            "validators": ["menu_generator.validators.is_anonymous"],
-        },
-        {
-            "name": "User",
-            "icon": "fa-user",
-            "url": "#",
-            "validators": ["menu_generator.validators.is_authenticated"],
-            "submenu": [
-                {
-                    "name": "Configuracion",
-                    "icon": "fa-sliders-h",
-                    "url": "/account/profile",
-                },
-                {
-                    "name": "Cerrar sesión",
-                    "icon": "fa-sign-out-alt",
-                    "url": "/logout/",
-                },
-            ],
-        },
     ],
 }
