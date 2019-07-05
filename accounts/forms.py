e8b9ce406aa92e5dba57034ee0390dbd3cda76da
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2019-07-05 13:17:56 -0400

    user admin creation form password validation

--- accounts/forms.py
+++ accounts/forms.py
@@ -24,6 +24,7 @@ class UserAdminCreationForm(forms.ModelForm):
         password2 = self.cleaned_data.get("password2")
         if password1 and password2 and password1 != password2:
             raise forms.ValidationError("Passwords don't match")
+        password_validation.validate_password(password2)
         return password2
 
     def save(self, commit=True):
