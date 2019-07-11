2bfd0873035ccc9e57b6c99a392c64874b66d1ab
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2019-07-11 11:25:54 -0400

    run tests

--- gremios/tests.py
+++ gremios/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase, Client
 
-from accounts.models import User
+from common.models import User
 from .models import Cartola, RutGremio
 
 
@@ -11,8 +11,27 @@ class ObjectsCreation(object):
         self.rut_gremios_list = ['15314993-3', '10634630-5', '9619616-4', '16747983-9', '15971053-K', '6597629-3']
         self.invalid_rut_gremios_list = ['1314993-3', '1063630-5', '961966-4', '1677983-9', '1971053-K', '659629-3']
         self.user = User.objects.create_user(rut='12245453-3', password='pass.1234')
-        self.staff_user = User.objects.create_staffuser(rut='16747983-9', password='pass.1234')
-        self.super_user = User.objects.create_superuser(rut='17256372-4', password='pass.1234')
+        self.user = User.objects.create_user(rut='7264437-9', 
+                                             email='test1@test.cl', 
+                                             first_name='Test', 
+                                             last_name='Testo', 
+                                             password='pass.1234')
+        self.superuser = User.objects.create_superuser(rut='12384351-7',
+                                                       email='superuser@test.cl',
+                                                       password='pass.1234')
+        self.staffuser = User.objects.create_staffuser(rut='16747983-9', 
+                                                        email='staffuser@test.cl',
+                                                        password='pass.1234')
+
+class GremiosPageTestCase(ObjectsCreation, TestCase):
+    def test_gremios_cartolas_page_guest(self):
+        response = self.client.get("/gremios/cartolas")
+        self.assertEqual(302, response.status_code)
+    
+    def test_gremios_cartolas_page_authenticated_user(self):
+        response = self.client.login(username=self.user.rut, password='pass.1234')
+        response = self.client.get("/gremios/cartolas")
+        self.assertEqual(200, response.status_code)
 
 class RutGremioModelTestCase(ObjectsCreation, TestCase):
     def test_create_rut_gremio(self):
@@ -35,12 +54,7 @@ class RutGremioModelTestCase(ObjectsCreation, TestCase):
                 not_valid = True
         self.assertEqual(not_valid, True)
 
-# class CartolaModelTestCase(ObjectsCreation, TestCase):
-    # def test_string_representation_user(self):
-    #     user = self.user
-    #     self.assertEqual(str(user.get_short_name()), user.rut)
-
-    # def test_create_user(self):
-    #     User.objects.create_user(rut='13064499-6', email='test@test.cl', full_name='Test Testo', password='pass.1234')
-    #     self.assertEqual(User.objects.all().filter(rut='13064499-6').count() != 0, True)
-
+class LoadCartolasTestCase(ObjectsCreation, TestCase):
+    def test_load_cartolas(self):
+        response = self.client.get("/gremios/load")
+        self.assertEqual(200, response.status_code)
