4343972fafa27031334fb70fd3c71f810c599745
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2019-07-17 10:41:00 -0400

    logging core app

--- core/views.py
+++ core/views.py
@@ -3,9 +3,17 @@ from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+from core.utils import get_client_ip
+
 
 class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = "core/home.html"
+    log_message = "GET Home Page User: {0} From: {1}"
     def get(self, request, *args, **kwargs):
-        logging.getLogger("info_logger").info("Home Page User: " + self.request.user.rut)
+        logging.getLogger("info_logger").info(self.log_message.format(request.user.rut, get_client_ip(request)))
         return render(request, self.template_name)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            logging.getLogger("error_logger").error(self.log_message.format(request.user, get_client_ip(request)))
+        return super().dispatch(request, *args, **kwargs)
