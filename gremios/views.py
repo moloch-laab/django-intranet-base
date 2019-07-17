f858a0e7a16abc5b957190f8ece80e92180fa27f
Francisco Bahamondes <francisco.bahamondes@hipodromo.local>
2019-07-17 10:54:02 -0400

    logging gremios app

--- gremios/views.py
+++ gremios/views.py
@@ -1,30 +1,39 @@
+import logging
 from django.views.generic.list import ListView
 from django.views.generic.base import TemplateView
 from django.utils.decorators import method_decorator
 from django.contrib.auth.mixins import LoginRequiredMixin
 # from django.shortcuts import render
 from django.http import HttpResponse
-import logging
 
 from .models import Cartola, RutGremio
-from core.utils import rows_from_txt
+from core.utils import rows_from_txt, get_client_ip
 
 class CartolasListView(LoginRequiredMixin, ListView):
     model = Cartola
+    log_message = "GET Cartola List Page User: {0} From: {1}"
 
     def get_queryset(self):
-        logging.getLogger("info_logger").info("Cartolas Gremios User: " + self.request.user.rut)
         rut_gremio = RutGremio.objects.filter(user_id=self.request.user).first()
+        logging.getLogger("info_logger").info(self.log_message.format(self.request.user.rut, get_client_ip(self.request)))
         return Cartola.objects.filter(rut_gremio_id=rut_gremio)
+
+    def dispatch(self, request, *args, **kwargs):
+        if not request.user.is_authenticated:
+            logging.getLogger("error_logger").error(self.log_message.format(request.user, get_client_ip(request)))
+        return super().dispatch(request, *args, **kwargs)
     
 class LoadCartolasView(TemplateView):
     def get(self, request, *args, **kwargs):
         model = Cartola()
         new_ruts = RutGremio.objects.create_rut_from_file()
         resultado = Cartola.objects.create_from_path()
+        log_message = "GET Load Cartolas Message: {0} From: {1}"
+
         if resultado:
-            logging.getLogger("info_logger").info('Cartolas cargadas')
+            logging.getLogger("info_logger").info(self.log_message.format('Cartolas cargadas', get_client_ip(request)))
             return HttpResponse('Cartolas cargadas')
+
         else:
-            logging.getLogger("error_logger").error('Error al cargar cartolas')
+            logging.getLogger("error_logger").error(self.log_message.format('Error al cargar cartolas', get_client_ip(request)))
             return HttpResponse('Error al cargar cartolas')
